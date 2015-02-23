46c91c247ade1dc195b9da2d858cf83149962972
Jayanth-Kulkarni <jayanthkul@gmail.com>
2015-02-23 09:00:33 +0530

    Changed

---
+++ stable matching using structure and arrays/README.md
@@ -0,0 +1,18 @@
+Stable Matching
+=========
+
+stable marriage problem (SMP) is the problem of finding a stable matching between two sets of elements given a set of preferences for each element. A matching is a mapping from the elements of one set to the elements of the other set. A matching is stable whenever it is not the case that both:
+
+* some given element  of the first matched set prefers some given element B of the second matched set over the element to which A is already matched, 
+* B also prefers A over the element to which B is already matched
+In other words, a matching is stable when there does not exist any alternative pairing (A, B) in which both A and B are individually better off than they would be with the element to which they are currently matched.
+
+The stable marriage problem is commonly stated as
+=============
+* Given n men and n women, where each person has ranked all members of the opposite sex with a unique number between 1 and n in order of preference, marry the men and women together such that there are no two people of opposite sex who would both rather have each other than their current partners. If there are no such people, all the marriages are "stable". (It is assumed that the participants are binary gendered and that marriages are not same-sex).
+
+* Algorithms for finding solutions to the stable marriage problem have applications in a variety of real-world situations, perhaps the best known of these being in the assignment of graduating medical students to their first hospital appointments. In 2012, the Nobel Prize in Economics was awarded to Lloyd S. Shapley and Alvin E. Roth "for the theory of stable allocations and the practice of market design."
+
+Reference
+===========
+* [Stable Marriage Problem](http://en.wikipedia.org/wiki/Stable_marriage_problem)
---
+++ stable matching using structure and arrays/main.cpp
@@ -0,0 +1,181 @@
+
+
+/********
+Dated : 22/02/15
+This code Belongs to Jayanth Prakash Kulkarni
+CSE 4th Semester BTech,
+M.S.Ramaiah Institute of Technology,Bangalore.
+********/
+
+
+
+#include <iostream>
+#define MAX 4
+#define FOR(i,j) for(int i=0;i<j;i++)
+using namespace std;
+
+typedef struct match
+{
+	int preference[MAX][MAX];
+	int flag[MAX];
+	int matched[MAX];
+	int currentpreference[MAX];
+	int current_women_index[MAX];//man's current woman index
+}match;
+
+void init_preference(match &m, match &w)
+{
+	cout << "Enter the preference of the men starting from 1 to "<<MAX+1<<"!"<< endl;
+	FOR(i, MAX)
+	{
+		cout << "Preference of man " << i + 1 << endl;
+		m.flag[i] = 0;
+		m.current_women_index[i] = 0;
+		FOR(j, MAX)
+		{
+		    int mem;
+		    cin>>mem;
+		    mem--;
+			m.preference[i][j]=mem;
+
+		}
+	}
+
+	cout << "Enter the preference of the women starting from 1 to "<<MAX+1<<"!"<< endl;
+	FOR(i, MAX)
+	{
+		cout << "Preference of woman " << i + 1 << endl;
+		w.flag[i] = -1;
+		FOR(j, MAX)
+		{
+            int mem;
+		    cin>>mem;
+		    mem--;
+			w.preference[i][j]=mem;
+		}
+	}
+}
+
+void disp_match(match &m)
+{
+	cout << "\n\n";
+	FOR(i, MAX)
+	{
+		FOR(j, MAX)
+		{
+			cout << m.preference[i][j]+1 << "\t";
+		}
+		cout << "\n";
+	}
+	cout << "\n\n";
+}
+
+void perform_match(match &m, match &w)
+{
+	int co;
+	int i = 0;
+	int j = 0;//ith man's current woman index
+	int overall_flag=0;
+
+	while ((i<MAX) && m.flag[i] == 0||(overall_flag<MAX))
+	{
+		while (i<MAX && m.flag[i] == 0)
+		{
+			if (overall_flag < MAX)
+			{
+				int num;
+				int current_woman = m.preference[i][m.current_women_index[i]];
+				int current_man = i;
+				if (w.flag[current_woman] == -1)//woman is free
+				{
+					m.matched[i] = current_woman;
+					m.flag[i] = 1;//he is engaged
+					w.currentpreference[current_woman] = i;
+					w.flag[current_woman] = 1;//she is engaged
+				}
+				else //woman is already engaged
+				{
+					int old_man_index;
+					int old_man = w.currentpreference[current_woman];//index of the already preferred one
+					FOR(i, MAX)
+					{
+						if (w.preference[current_woman][i] == current_man)//
+						{
+							num = i;
+
+						}
+						if (w.preference[current_woman][i] == old_man)
+						{
+							old_man_index = i;
+						}
+					}
+					if (num<old_man_index)
+					{
+						m.flag[old_man] = 0;
+						m.flag[current_man] = 1;
+						m.matched[current_man] = current_woman;
+						w.currentpreference[current_woman] = current_man;
+						i = old_man;
+						m.current_women_index[i]++;
+						j = m.current_women_index[i];
+						overall_flag = 0;
+						FOR(j, MAX)
+						{
+							if (m.flag[j])
+								overall_flag++;
+						}
+						continue;
+					}
+					else
+					{
+						i = old_man;
+						m.current_women_index[i]++;
+						j = m.current_women_index[i];
+						overall_flag = 0;
+						FOR(j, MAX)
+						{
+							if (m.flag[j])
+								overall_flag++;
+						}
+						continue;
+					}
+				}
+				overall_flag = 0;
+				FOR(j, MAX)
+				{
+					if (m.flag[j])
+						overall_flag++;
+				}
+
+				 i++;
+				 co = i;
+			}
+		}
+
+		i++;
+
+	}
+}
+void display_result(match &m)
+{
+	FOR(i, MAX)
+	{
+		cout << "Man" << i << " is mapped with " << m.matched[i] << endl;
+	}
+	cin.clear();
+	cin.ignore();
+}
+
+int main()
+{
+	match men, women;
+	init_preference(men, women);
+	disp_match(men);
+	disp_match(women);
+	perform_match(men, women);
+	display_result(men);
+	cin.clear();
+	cin.ignore();
+	return 0;
+
+}
